c689b0cfe6b5cadec1d8eb058528c3536b9c11c8
Marlene Buch <marlene.buch@ku.de>
2025-09-07 22:12:18 -0400

    comment out debug directory creation & reorganize materials
    - comment out ica_data subdirectory creation in MADE pipeline
    - remove debug script MADE_pipeline_SocCEr_step14debug.m
    - update batch preprocessing script
    - reorganize materials folder structure
    - update project structure documentation

--- code/preprocessing-eeg/MADE_pipeline_SocCEr.m
+++ code/preprocessing-eeg/MADE_pipeline_SocCEr.m
@@ -229,8 +229,8 @@ output_format = 1; % 1 = .set (EEGLAB data structure), 2 = .mat (Matlab data str
 subjects_to_process = string(split(subjects, "/"));
 subjects_to_process = subjects_to_process(subjects_to_process~=""); %nvm not necessary
 subjects_to_process = strcat("sub-", subjects_to_process);
-% for file_locater_counter = 1:length(subjects_to_process) % This for loop lists the folders containing the main data files
-parfor file_locater_counter = 1:length(subjects_to_process) %1:4
+for file_locater_counter = 1:length(subjects_to_process) % This for loop lists the folders containing the main data files
+% parfor file_locater_counter = 1:length(subjects_to_process) %1:4
         try
         subjStart = tic;
         rawdata_location = fullfile(rawdata_location_parent, subjects_to_process(file_locater_counter));
@@ -839,15 +839,15 @@ parfor file_locater_counter = 1:length(subjects_to_process) %1:4
             length_ica_data=EEG_copy.trials; % length of data (in second) fed into ICA
             EEG_copy = eeg_checkset(EEG_copy);
             % outcomment next line to skip ICA (out-comment following pop_saveset too and un-comment pop_loadset!) I Marlene 2025
-            EEG_copy = pop_runica(EEG_copy, 'icatype', 'runica', 'extended', 1, 'stop', 1E-7, 'interupt','off');
+            % EEG_copy = pop_runica(EEG_copy, 'icatype', 'runica', 'extended', 1, 'stop', 1E-7, 'interupt','off');
 
             % save data here for training purposes only (usually do not save here)
             % only doing this to allow for skipping the full run of ica
-            EEG_copy = pop_saveset(EEG_copy, 'filename', strrep(datafile_names{subject}, ext, '_ica_data_immediate.set'),'filepath', [output_location filesep 'ica_data' filesep ]); % save .set format
+            % EEG_copy = pop_saveset(EEG_copy, 'filename', strrep(datafile_names{subject}, ext, '_ica_data_immediate.set'),'filepath', [output_location filesep 'ica_data' filesep ]); % save .set format
 
             % load data here for training purposes only (usually do not save here)
             % only doing this to allow for skipping the full run of ica
-            % EEG_copy = pop_loadset( 'filename', strrep(datafile_names{subject}, ext, '_ica_data_immediate.set'), 'filepath', [output_location filesep 'ica_data' filesep]);
+            EEG_copy = pop_loadset( 'filename', strrep(datafile_names{subject}, ext, '_ica_data_immediate.set'), 'filepath', [output_location filesep 'ica_data' filesep]);
 
             % Find the ICA weights that would be transferred to the original dataset
             ICA_WINV=EEG_copy.icawinv;
@@ -872,10 +872,6 @@ parfor file_locater_counter = 1:length(subjects_to_process) %1:4
             EEG_copy = EEG;
             EEG_copy = eeg_regepochs(EEG_copy,'recurrence', 1, 'limits',[0 1], 'rmbase', [NaN], 'eventtype', '999'); % insert temporary marker 1 second apart and create epochs
             EEG_copy = eeg_checkset(EEG_copy);
-
-            %% Marlene 2025 I debug start
-            % diagnostic script to test findpeaks path conflict
-            % add this code right before line 875 in MADE_pipeline_SocCEr.m
 
             % Marlene 2025 I fix chronux findpeaks path conflict before ICA artifact detection
             chronux_findpeaks = which('findpeaks');
